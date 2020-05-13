437d9d55e318ad124d23d598c581b6c1d3bb7c0a
JBSal8 <55267786+JBSal8@users.noreply.github.com>
2020-05-12 22:03:16 -0700

    Add files via upload

--- bullet.py
+++ bullet.py
@@ -2,19 +2,20 @@ import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    """A Class to manage bullets fired from the ship"""
+    """A class to manage bullets fired from the ship."""
 
     def __init__(self, ai_settings, screen, ship):
-        """Create a bullet object at the ship's current position."""
+        """Create a bullet object, at the ship's current position."""
         super(Bullet, self).__init__()
         self.screen = screen
 
-        """Create a Bullet rect at (0,0) and then set correct position."""
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,ai_settings.bullet_height)
+        # Create bullet rect at (0, 0), then set correct position.
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+            ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
-
-        #Store the bullet's position as a decimal value.
+        
+        # Store a decimal value for the bullet's position.
         self.y = float(self.rect.y)
 
         self.color = ai_settings.bullet_color
@@ -22,13 +23,11 @@ class Bullet(Sprite):
 
     def update(self):
         """Move the bullet up the screen."""
-        #Update the decimal position of the bullet.
+        # Update the decimal position of the bullet.
         self.y -= self.speed_factor
-        #Update the rect position.
+        # Update the rect position.
         self.rect.y = self.y
 
     def draw_bullet(self):
         """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
-        
-        
